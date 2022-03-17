bbacb3cd2213fd638685a3d799554340378d32b9
StarBound6494 <theleaderofthefearless@gmail.com>
2022-03-17 15:33:24 -0400

    3.3.1
    https://github.com/StarBound6494/NewGods/releases/tag/3.3.0

---
+++ MarryGift.java
@@ -0,0 +1,50 @@
+package com.fluff_stuff.newgods.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.fluff_stuff.newgods.NewGods;
+
+public class MarryGift implements CommandExecutor{
+
+	public boolean onCommand(CommandSender sender, Command arg1, String arg2, String[] arg3) {
+		if (sender instanceof Player) {
+			Player player = (Player) sender;
+			if(player.hasPermission("newgods.marrygift")==false){
+				player.sendMessage(ChatColor.DARK_RED+"Sorry but you're not permitted to do this.");
+				return true;
+			}
+			int playerID = NewGods.data.getPlayerID(player.getName());
+			String spouce = NewGods.data.playerPartner.get(playerID);
+			if(spouce.equals("null")){
+				player.sendMessage(ChatColor.DARK_RED+"You need to be married to do this.");
+				return true;
+			}
+			Player p = Bukkit.getServer().getPlayer(spouce);
+			if(p==null){
+				player.sendMessage(ChatColor.DARK_RED+"Your partner isn't online.");
+				return true;
+			}
+			ItemStack gift = player.getInventory().getItemInMainHand();
+			if(gift != null){
+				if( p.getInventory().firstEmpty() == -1 ){ //a value of -1 means the inventory is full
+					player.sendMessage(ChatColor.RED + "Couldn't gift the item to your Partner!");
+				}
+				else{
+					p.getInventory().addItem(gift);
+					p.sendMessage(ChatColor.AQUA + player.getName() + " gifted you " + gift.toString() + ".");
+					player.getInventory().getItemInMainHand().setAmount(0);
+					player.sendMessage(ChatColor.AQUA + "Gifted the item you were holding.");
+				}
+			}
+		}
+		return true;
+	}
+
+}
